1155f29aad9de0585a532b767104ee03bb37856f
Luis Garcia <gbluisman@gmail.com>
2023-06-08 21:00:36 -0600

    Updates

---
+++ precache-manifest.bf1680db7e177c1d4fd8833a958cfee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d8d4ac4557c68a0771",
+    "url": "/app_recetas/static/js/main.a3d8d4ac.chunk.js"
+  },
+  {
+    "revision": "ec766d5775bb0d4723b4",
+    "url": "/app_recetas/static/css/1.4ed8bad6.chunk.css"
+  },
+  {
+    "revision": "ec766d5775bb0d4723b4",
+    "url": "/app_recetas/static/js/1.ec766d57.chunk.js"
+  },
+  {
+    "revision": "f39b3707d79410ca4e51",
+    "url": "/app_recetas/static/js/runtime~main.f39b3707.js"
+  },
+  {
+    "revision": "6a68b676dc5007c3ae19849cd747ab78",
+    "url": "/app_recetas/static/media/recipe.6a68b676.png"
+  },
+  {
+    "revision": "baf6f00b24960a483c326f325ed46c54",
+    "url": "/app_recetas/index.html"
+  }
+];
